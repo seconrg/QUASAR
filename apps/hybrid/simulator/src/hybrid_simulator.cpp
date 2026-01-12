18d267452b9dc34062781c72209b44b31684ece2
seconrg_laptop <seconrg@github.com>
2026-01-12 06:58:01 -0800

    Fix error

--- apps/hybrid/simulator/src/hybrid_simulator.cpp
+++ apps/hybrid/simulator/src/hybrid_simulator.cpp
@@ -32,6 +32,7 @@ using namespace quasar;
 int main(int argc, char** argv) {
     Config config{};
     config.title = "Hybrid Simulator";
+    config.sortTransparent = false;
 
     args::ArgumentParser parser(config.title);
     args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
